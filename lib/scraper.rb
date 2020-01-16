f2df107c42275bccb29930b57226c9ebe138cb27
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-16 02:24:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,7 +9,7 @@ class Scraper
   def get_page
 
     Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-
+  end
     # doc.css(".post").each do |post|
     #   course = Course.new
     #   course.title = post.css("h2").text
